f7713723bcbda3b4f0a48e700ab744221465877f
Nick Krzysiak <krzysiaknick@gmail.com>
2023-11-04 11:50:05 -0500

    test

--- add-commit-push.py
+++ add-commit-push.py
@@ -13,8 +13,10 @@ else:
 
 
 commit_user_response = input("would you like to commit? Y/N:  ")
+message = input("Enter your commit message: ")
+        
 if commit_user_response == "y":
-    os.system("git commit")
+    os.system("git commit -m" + message)
 elif commit_user_response == "n":
      os.abort
 else:
