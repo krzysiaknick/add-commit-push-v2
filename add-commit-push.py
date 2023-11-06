1a51ee501587621a0699d028c98c819f6ab91def
Nick Krzysiak <krzysiaknick@gmail.com>
2023-11-05 18:43:44 -0600

    yes

--- add-commit-push.py
+++ add-commit-push.py
@@ -7,7 +7,7 @@ add_user_response = input("would you like to add? Y/N ('-f')to force:  ")
 if add_user_response == "y":
     os.system("git add -A")
 elif add_user_response == "n":
-     os.abort
+     sys.exit()
 elif add_user_response == "-f":
      os.system("git add -A")
      os.system("git commit -m force")
@@ -17,6 +17,7 @@ elif add_user_response == "-f":
 
 else:
      print("Error")
+     sys.exit()
 
 
 commit_user_response = input("would you like to commit? Y/N:  ")
@@ -25,16 +26,18 @@ message = input("Enter your commit message (Use dashes (-) as spaces): ")
 if commit_user_response == "y":
     os.system("git commit -m" + message)
 elif commit_user_response == "n":
-     os.abort
+     sys.exit()
 else:
      print("Error")
+     sys.exit()
 
 push_user_response = input("Would you like to push these changes? Y/N: ")
 if commit_user_response == "y":
     os.system("git push")
 elif commit_user_response == "n":
-     os.abort
+     sys.exit()
 else:
      print("Error")
+     sys.exit()
 
       
